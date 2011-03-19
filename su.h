f335301b0deadf49c033721d7dc55a5f46036a24
Jan Vlnas <git@jan.vlnas.cz>
2011-03-19 22:35:28 +0100

    Removed unused constants from header

--- su.h
+++ su.h
@@ -1,14 +1,5 @@
-#ifndef SU_h 
+#ifndef SU_h
 #define SU_h 1
-
-#define REQUESTOR_PACKAGE   "com.noshufou.android.su"
-#define REQUESTOR_CLASS     "SuRequest"
-
-#define REQUESTOR_DATA_PATH "/data/data/" REQUESTOR_PACKAGE
-#define REQUESTOR_CACHE_PATH REQUESTOR_DATA_PATH "/cache"
-
-#define REQUESTOR_DATABASES_PATH REQUESTOR_DATA_PATH "/databases"
-#define REQUESTOR_DATABASE_PATH REQUESTOR_DATABASES_PATH "/permissions.sqlite"
 
 #define DEFAULT_COMMAND "/system/bin/sh"
 
@@ -25,8 +16,6 @@ struct su_request {
     unsigned uid;
     char *command;
 };
-
-extern int send_intent(struct su_initiator *from, struct su_request *to, const char *socket_path, int type);
 
 #if 0
 #undef LOGE
